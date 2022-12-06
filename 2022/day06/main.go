dc460a8579cc12e6987e5c68d647053e9298464a
Daniel Ben-Zion <dbzion@gmail.com>
2022-12-06 19:29:18 +0900

    day 6

--- 2022/day06/main.go
+++ 2022/day06/main.go
@@ -7,10 +7,35 @@ import (
 )
 
 func main() {
-	contents, err := os.ReadFile(os.Args[1])
+	code, err := os.ReadFile(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(contents)
+	var stop int = -1
+	for i := 3; i < len(code); i++ {
+		if checkSequenceNonRepeating(code, i, 4) {
+			stop = i + 1
+			break
+		}
+	}
+	fmt.Printf("Part One: Start packet read starting at position %d\n", stop)
+
+	stop = -1
+	for i := 13; i < len(code); i++ {
+		if checkSequenceNonRepeating(code, i, 14) {
+			stop = i + 1
+			break
+		}
+	}
+	fmt.Printf("Part Two: Start message read starting at position %d\n", stop)
+
+}
+
+func checkSequenceNonRepeating(code []byte, start, n int) bool {
+	var hashSet = make(map[byte]struct{})
+	for i := 0; i < n; i++ {
+		hashSet[code[start-i]] = struct{}{}
+	}
+	return len(hashSet) == n
 }

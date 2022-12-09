6e244dc53f7cf7d6c883a1fc6f3061493b293bd4
Daniel Ben-Zion <dbzion@gmail.com>
2022-12-09 19:50:52 +0900

    day 9

--- 2022/day08/main.go
+++ 2022/day08/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -12,5 +14,113 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(contents)
+	lines := strings.Split(string(contents), "\n")
+	arr := [][]int{}
+
+	for _, line := range lines {
+		row := []int{}
+		for _, ch := range line {
+			i, err := strconv.Atoi(string(ch))
+			if err != nil {
+				log.Fatal(err)
+			}
+			row = append(row, i)
+		}
+		arr = append(arr, row)
+	}
+
+	var totalVisible int
+
+	for x := range arr {
+		for y := range arr[x] {
+			if isVisible(arr, x, y) {
+				totalVisible++
+			}
+		}
+	}
+
+	fmt.Printf("Part One: %d trees are visible\n", totalVisible)
+
+	// Part Two
+	var maxScenicScore, ss int
+
+	for x := range arr {
+		for y := range arr[x] {
+			ss = scenicScore(arr, x, y)
+			if ss > maxScenicScore {
+				maxScenicScore = ss
+			}
+		}
+	}
+
+	fmt.Printf("Part Two: Maximum scenic score found was %d\n", maxScenicScore)
+
+}
+
+func isVisible(arr [][]int, x, y int) bool {
+	// Not the most efficient since we continue scanning even
+	// if it was visible in one direction, but okay.
+
+	// scan up
+	var visU, visD, visL, visR = true, true, true, true
+	for xt := 0; xt < x; xt++ {
+		if arr[xt][y] >= arr[x][y] {
+			visU = false
+		}
+	}
+	// scan down
+	for xt := x + 1; xt < len(arr); xt++ {
+		if arr[xt][y] >= arr[x][y] {
+			visD = false
+		}
+	}
+	// scan left
+	for yt := 0; yt < y; yt++ {
+		if arr[x][yt] >= arr[x][y] {
+			visL = false
+		}
+	}
+	// scan right
+	for yt := y + 1; yt < len(arr[0]); yt++ {
+		if arr[x][yt] >= arr[x][y] {
+			visR = false
+		}
+	}
+	return visU || visD || visL || visR
+}
+
+func scenicScore(arr [][]int, x, y int) int {
+	// Not the most efficient since we continue scanning even
+	// if it was visible in one direction, but okay.
+
+	// scan up
+	var canSeeU, canSeeD, canSeeL, canSeeR int
+	for xt := x - 1; xt >= 0; xt-- {
+		canSeeU++
+		if arr[xt][y] >= arr[x][y] {
+			break
+		}
+	}
+	// scan down
+	for xt := x + 1; xt < len(arr); xt++ {
+		canSeeD++
+		if arr[xt][y] >= arr[x][y] {
+			break
+		}
+	}
+	// scan left
+	for yt := y - 1; yt >= 0; yt-- {
+		canSeeL++
+		if arr[x][yt] >= arr[x][y] {
+			break
+		}
+	}
+	// scan right
+	for yt := y + 1; yt < len(arr[0]); yt++ {
+		canSeeR++
+		if arr[x][yt] >= arr[x][y] {
+			break
+		}
+	}
+	return canSeeU * canSeeD * canSeeL * canSeeR
 }
--- 2022/day09/main.go
+++ 2022/day09/main.go
@@ -4,7 +4,59 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
+
+type point struct {
+	x, y int
+}
+
+func (p *point) move(dx, dy int) {
+	p.x += dx
+	p.y += dy
+}
+
+func offset(p1, p2 point) (int, int) {
+	return p2.x - p1.x, p2.y - p1.y
+}
+
+func (t *point) follow(h point) {
+	dx, dy := offset(*t, h)
+	if dx >= 2 {
+		if dy == 0 {
+			t.move(1, 0)
+		} else if dy > 0 {
+			t.move(1, 1)
+		} else if dy < 0 {
+			t.move(1, -1)
+		}
+	} else if dx <= -2 {
+		if dy == 0 {
+			t.move(-1, 0)
+		} else if dy > 0 {
+			t.move(-1, 1)
+		} else if dy < 0 {
+			t.move(-1, -1)
+		}
+	} else if dy >= 2 {
+		if dx == 0 {
+			t.move(0, 1)
+		} else if dx > 0 {
+			t.move(1, 1)
+		} else if dx < 0 {
+			t.move(-1, 1)
+		}
+	} else if dy <= -2 {
+		if dx == 0 {
+			t.move(0, -1)
+		} else if dx > 0 {
+			t.move(1, -1)
+		} else if dx < 0 {
+			t.move(-1, -1)
+		}
+	}
+
+}
 
 func main() {
 	contents, err := os.ReadFile(os.Args[1])
@@ -12,5 +64,59 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(contents)
+	var (
+		head, tail point
+		dir        string
+		nSteps     int
+		hashSet1   = make(map[point]struct{})
+		hashSet2   = make(map[point]struct{})
+	)
+
+	for _, instruction := range strings.Split(string(contents), "\n") {
+		fmt.Sscanf(instruction, "%s %d", &dir, &nSteps)
+		for i := 0; i < nSteps; i++ {
+			if dir == "U" {
+				head.move(0, 1)
+			} else if dir == "D" {
+				head.move(0, -1)
+			} else if dir == "R" {
+				head.move(1, 0)
+			} else if dir == "L" {
+				head.move(-1, 0)
+			}
+
+			tail.follow(head)
+			hashSet1[tail] = struct{}{}
+		}
+	}
+
+	fmt.Printf("Part One: %d tiles visited by the tail of the rope\n", len(hashSet1))
+
+	// Part Two
+	ropeLength := 10
+	var rope = make([]point, ropeLength)
+	for _, instruction := range strings.Split(string(contents), "\n") {
+		fmt.Sscanf(instruction, "%s %d", &dir, &nSteps)
+
+		for i := 0; i < nSteps; i++ {
+			if dir == "U" {
+				rope[0].move(0, 1)
+			} else if dir == "D" {
+				rope[0].move(0, -1)
+			} else if dir == "R" {
+				rope[0].move(1, 0)
+			} else if dir == "L" {
+				rope[0].move(-1, 0)
+			}
+
+			for i := 1; i < ropeLength; i++ {
+				rope[i].follow(rope[i-1])
+			}
+
+			hashSet2[rope[len(rope)-1]] = struct{}{}
+		}
+	}
+
+	fmt.Printf("Part Two: %d tiles visited by the tail of the rope\n", len(hashSet2))
+
 }

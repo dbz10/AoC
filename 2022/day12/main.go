1f6cfdd08fdade548ec30f993a11b15e2482d556
Daniel Ben-Zion <dbzion@gmail.com>
2022-12-14 21:36:17 +0900

    day 12, day 13 needed help :(

--- 2022/day12/main.go
+++ 2022/day12/main.go
@@ -4,7 +4,56 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
+	"strings"
 )
+
+const initTd = 100000000
+
+type node struct {
+	value            int
+	validNeighbors   []point
+	reverseNeighbors []point
+}
+
+func (n *node) addValidNeighbor(other point) {
+	n.validNeighbors = append(n.validNeighbors, other)
+}
+
+type point struct {
+	x, y int
+}
+
+var charValue = map[string]int{
+	"a": 1,
+	"b": 2,
+	"c": 3,
+	"d": 4,
+	"e": 5,
+	"f": 6,
+	"g": 7,
+	"h": 8,
+	"i": 9,
+	"j": 10,
+	"k": 11,
+	"l": 12,
+	"m": 13,
+	"n": 14,
+	"o": 15,
+	"p": 16,
+	"q": 17,
+	"r": 18,
+	"s": 19,
+	"t": 20,
+	"u": 21,
+	"v": 22,
+	"w": 23,
+	"x": 24,
+	"y": 25,
+	"z": 26,
+	"S": 1,
+	"E": 26,
+}
 
 func main() {
 	contents, err := os.ReadFile(os.Args[1])
@@ -12,5 +61,128 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(contents)
+	nodes := map[point]*node{}
+	rows := strings.Split(string(contents), "\n")
+
+	letterGrid := [][]string{}
+	for _, row := range rows {
+		letterRow := []string{}
+		for _, char := range row {
+			letterRow = append(letterRow, string(char))
+		}
+		letterGrid = append(letterGrid, letterRow)
+	}
+
+	xShape := len(letterGrid)
+	yShape := len(letterGrid[0])
+
+	var start, end point
+
+	// Construct graph in two stages.
+	// First, initialize all of the nodes
+	for x, row := range letterGrid {
+		for y, v := range row {
+			nodes[point{x, y}] = &node{value: charValue[v]}
+			if v == "S" {
+				start = point{x, y}
+			}
+			if v == "E" {
+				end = point{x, y}
+			}
+		}
+	}
+
+	// Now, add connections
+	for loc := range nodes {
+		allNeighbors := getNeighborIndices(loc.x, loc.y, xShape, yShape)
+		for _, neighbor := range allNeighbors {
+			if nodes[neighbor].value <= nodes[loc].value+1 {
+				nodes[loc].addValidNeighbor(neighbor)
+			}
+		}
+	}
+
+	pathLength := dijkstra(nodes, start, end)
+	fmt.Printf("Part One: %d steps are required to reach the high point\n", pathLength)
+
+	// Part Two
+	// I know this is highly inefficient but I just want to move on.
+
+	startingPointCandidates := []point{}
+	for point, node := range nodes {
+		if node.value == 1 {
+			startingPointCandidates = append(startingPointCandidates, point)
+		}
+	}
+
+	optimizedPathLength := pathLength
+	for _, candidate := range startingPointCandidates {
+		distance := dijkstra(nodes, candidate, end)
+		if distance < optimizedPathLength {
+			optimizedPathLength = distance
+		}
+	}
+
+	fmt.Printf("Part Two: The shortest path from base elevation to the optimal reception point takes %d steps\n", optimizedPathLength)
+
+}
+
+func getNeighborIndices(x, y, xShape, yShape int) []point {
+	neighboringIndices := []point{}
+	for _, dx := range []int{-1, 1} {
+		if x+dx >= 0 && x+dx < xShape {
+			neighboringIndices = append(neighboringIndices, point{x + dx, y})
+		}
+	}
+
+	for _, dy := range []int{-1, 1} {
+		if y+dy >= 0 && y+dy < yShape {
+			neighboringIndices = append(neighboringIndices, point{x, y + dy})
+		}
+	}
+
+	return neighboringIndices
+}
+
+func dijkstra(graph map[point]*node, start, end point) int {
+	// Set up a set of all unvisited nodes
+	unvisitedNodes := []point{}
+	for key := range graph {
+		unvisitedNodes = append(unvisitedNodes, key)
+	}
+	distanceToNodes := map[point]int{}
+	// set initial distance super high, technically infinity but for a limited size graph, this is more than enough
+	for key := range graph {
+		distanceToNodes[key] = initTd
+	}
+	distanceToNodes[start] = 0
+	sort.Slice(unvisitedNodes, func(i, j int) bool { return distanceToNodes[unvisitedNodes[i]] < distanceToNodes[unvisitedNodes[j]] })
+
+	// ok it's not the most clear interface with jumping back and forth between
+	// point <-> node but anyways, it works
+
+	for contains(unvisitedNodes, end) && distanceToNodes[unvisitedNodes[0]] < initTd {
+		current := unvisitedNodes[0]
+		unvisitedNodes = unvisitedNodes[1:]
+		for _, neighbor := range graph[current].validNeighbors {
+			if contains(unvisitedNodes, neighbor) && distanceToNodes[neighbor] > distanceToNodes[current]+1 {
+				distanceToNodes[neighbor] = distanceToNodes[current] + 1
+			}
+		}
+		sort.Slice(unvisitedNodes, func(i, j int) bool { return distanceToNodes[unvisitedNodes[i]] < distanceToNodes[unvisitedNodes[j]] })
+		if len(unvisitedNodes) == 0 {
+			break
+		}
+	}
+
+	return distanceToNodes[end]
+}
+
+func contains(unvisitedNodes []point, p point) bool {
+	for _, pp := range unvisitedNodes {
+		if pp == p {
+			return true
+		}
+	}
+	return false
 }
--- 2022/day13/main.go
+++ 2022/day13/main.go
@@ -3,8 +3,52 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
+
+// A packet can have EITHER a value OR children, but not both
+type packet struct {
+	value    []int // just for the ability to have an empty slice, though it will have only 1 or zero elements
+	children []*packet
+	parent   *packet
+}
+
+func (p packet) isTerminal() bool {
+	return len(p.children) == 0
+}
+
+func (p packet) valueNonEmpty() bool {
+	return len(p.value) > 0
+}
+
+func (p packet) valueEmpty() bool {
+	return !p.valueNonEmpty()
+}
+
+func (p packet) String() string {
+	// Depth First
+	ll := len(p.children)
+	outputString := ""
+	if p.isTerminal() && p.valueNonEmpty() {
+		outputString += fmt.Sprint(p.value[0])
+	} else if p.isTerminal() && p.valueEmpty() {
+		outputString += ""
+	} else {
+		outputString += "["
+		for i, child := range p.children {
+			outputString += child.String()
+			if i < ll-1 {
+				outputString += ","
+			}
+		}
+		outputString += "]"
+	}
+	return outputString
+}
 
 func main() {
 	contents, err := os.ReadFile(os.Args[1])
@@ -12,5 +56,163 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(contents)
+	allPairs := strings.Split(string(contents), "\n\n")
+	packets := [][2]packet{}
+	for _, p := range allPairs {
+		pairs := strings.Split(p, "\n")
+		packets = append(packets, [2]packet{parsePacket(pairs[0]), parsePacket(pairs[1])})
+	}
+
+	results := []bool{}
+	for _, p := range packets {
+		res := compare(p[0], p[1])
+		// Because I know 0 will not happen
+		if res == 1 {
+			results = append(results, true)
+		} else {
+			results = append(results, false)
+		}
+	}
+
+	var sum int
+	for i := range results {
+		if results[i] {
+			sum += i + 1
+		}
+	}
+	fmt.Println("Part One:", sum)
+
+	// Part Two
+	// is easy thanks to my great pain in part one
+	flattened := []packet{}
+	for _, p := range packets {
+		flattened = append(flattened, p[0], p[1])
+	}
+	flattened = append(flattened, parsePacket("[[2]]"))
+	flattened = append(flattened, parsePacket("[[6]]"))
+
+	sort.Slice(flattened, func(i, j int) bool { return compare(flattened[i], flattened[j]) == 1 })
+	var prod int = 1
+	for i, f := range flattened {
+		if fmt.Sprint(f) == "[[2]]" || fmt.Sprint(f) == "[[6]]" {
+			prod *= (i + 1)
+		}
+	}
+	fmt.Println("Part Two:", prod)
+}
+
+func parsePacket(s string) packet {
+	p := packet{}
+	segments := strings.Split(strings.ReplaceAll(strings.ReplaceAll(s, "[", "[,"), "]", ",]"), ",") // vomit
+	currentPointer := &p
+	for _, block := range segments {
+		if block == "[" {
+			childPointer := &packet{parent: currentPointer}
+			currentPointer.children = append(currentPointer.children, childPointer)
+			currentPointer = childPointer
+		} else if block == "]" {
+			currentPointer = currentPointer.parent
+		} else if block == "" {
+			childPointer := &packet{value: []int{}, parent: currentPointer}
+			currentPointer.children = append(currentPointer.children, childPointer)
+		} else {
+			// then it had better be a number
+			newVal, err := strconv.Atoi(block)
+			if err != nil {
+				log.Fatal(err)
+			}
+			childPointer := &packet{value: []int{newVal}, parent: currentPointer}
+			currentPointer.children = append(currentPointer.children, childPointer)
+		}
+	}
+	// Since we always have the outer containing list
+	return *p.children[0]
+}
+
+func compare(l, r packet) int {
+	// This function is absolutely horrible
+	// Overall idea: As soon as we get a definitive comparison,
+	// break out of everything and return it. Until then, continue.
+	// It seems that structurally I have to allow for the possibility
+	// That the entire comparison is inconclusive, even though it
+	// might not ever happen in practice
+	// So the output will be -1, 0, 1
+
+	// If L and R are both terminal nodes, compare the values,
+	// after checking that the values are not empty
+	if l.isTerminal() && r.isTerminal() {
+		return compareTerminalValues(l, r)
+	} else if l.isTerminal() && !r.isTerminal() {
+		// Then l is a terminal node and we need to compare the value to r's child
+		if l.valueEmpty() {
+			return 1
+		}
+		// otherwise, compare the value to (up to) r's first child-value
+		cv := compare(l, *r.children[0])
+		if cv == 1 {
+			return 1
+		} else if cv == 0 && len(r.children) > 1 {
+			return 1
+		} else {
+			return -1
+		}
+	} else if !l.isTerminal() && r.isTerminal() {
+		// Then r is a terminal node and we need to compare the value to l's child
+		if r.valueEmpty() {
+			return -1
+		}
+		// otherwise, compare the value to (up to) l's first child-value
+		cv := compare(*l.children[0], r)
+		if cv == 1 {
+			return 1
+		} else if cv == 0 && len(l.children) > 1 {
+			return -1
+		} else {
+			return -1
+		}
+	} else {
+		// Otherwise, we start comparing children one by one
+		ll := len(l.children)
+		rr := len(r.children)
+		for i := 0; i < max(ll, rr); i++ {
+			if ll == i {
+				// Then L has run out of values
+				return 1
+			} else if rr == i {
+				// Then R has run out of values
+				return -1
+			} else {
+				cv := compare(*l.children[i], *r.children[i])
+				if cv == 0 {
+					continue
+				}
+				return cv
+			}
+		}
+		return 0
+	}
+}
+
+func compareTerminalValues(l, r packet) int {
+	if l.valueNonEmpty() && r.valueNonEmpty() {
+		return tril(l.value[0], r.value[0])
+	} else if l.valueNonEmpty() && r.valueEmpty() {
+		return -1
+	} else if l.valueEmpty() && r.valueNonEmpty() {
+		return 1
+	}
+	return 0
+}
+
+func tril(a, b int) int {
+	if a < b {
+		return 1
+	} else if b < a {
+		return -1
+	}
+	return 0
+}
+
+func max(a, b int) int {
+	return int(math.Max(float64(a), float64(b)))
 }

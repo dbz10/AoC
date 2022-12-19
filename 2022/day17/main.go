9ae2dcb0f8fbe72929cb6a09f693eb3ccbcced8d
Daniel Ben-Zion <dbzion@gmail.com>
2022-12-20 07:38:32 +0900

    include some WIP

--- 2022/day17/main.go
+++ 2022/day17/main.go
@@ -1,10 +1,39 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
+
+type coord struct {
+	x, y int
+}
+
+func (c coord) plus(other coord) coord {
+	return coord{c.x + other.x, c.y + other.y}
+}
+
+type block struct {
+	origin  coord   // bottom left segment of the square which encloses the block. does not have to contain a rock
+	offsets []coord // displacement to all the other segments in the block
+}
+
+type state struct {
+	// blockStateEncoding int
+	gustIndex int
+	nextRock  int
+}
+
+func (b block) String() string {
+	base := fmt.Sprintf("Origin: %v, Positions: ", b.origin)
+	for _, o := range b.offsets {
+		base += fmt.Sprintf("%v, ", b.origin.plus(o))
+	}
+	return base
+}
 
 func main() {
 	contents, err := os.ReadFile(os.Args[1])
@@ -12,5 +41,223 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(contents)
+	rockTypes := []block{
+		{
+			origin:  coord{0, 0},
+			offsets: []coord{{0, 0}, {1, 0}, {2, 0}, {3, 0}},
+		},
+		{
+			origin:  coord{0, 0},
+			offsets: []coord{{0, 1}, {1, 0}, {1, 1}, {1, 2}, {2, 1}},
+		},
+		{
+			origin:  coord{0, 0},
+			offsets: []coord{{0, 0}, {1, 0}, {2, 0}, {2, 1}, {2, 2}},
+		},
+		{
+			origin:  coord{0, 0},
+			offsets: []coord{{0, 0}, {0, 1}, {0, 2}, {0, 3}},
+		},
+		{
+			origin:  coord{0, 0},
+			offsets: []coord{{0, 0}, {0, 1}, {1, 0}, {1, 1}},
+		},
+	}
+
+	wind := strings.Split(string(contents), "")
+	var maxRockHeight, rockIterator, landedRocks int
+	allRock := map[coord]bool{}
+	for x := 0; x < 7; x++ {
+		allRock[coord{x, 0}] = true
+	}
+	rock := newRock(rockIterator, maxRockHeight, rockTypes)
+	rockIterator++
+
+	for round := 0; round < 1000000; round++ {
+		if landedRocks == 2022 {
+			break
+		}
+
+		windDirection := wind[round%len(wind)]
+		rock = tryMoveLR(rock, allRock, windDirection)
+		rock, err = tryMoveDown(rock, allRock)
+
+		if err != nil {
+			// update the set with all rock locations and max rock height
+			landedRocks++
+			for _, offset := range rock.offsets {
+				allRock[rock.origin.plus(offset)] = true
+				maxRockHeight = max(rock.origin.plus(offset).y, maxRockHeight)
+			}
+			// check if we've reached a cycle. we only need to measure this once.
+
+			// generate a new rock
+			rock = newRock(rockIterator%len(rockTypes), maxRockHeight, rockTypes)
+			rockIterator++
+		}
+
+	}
+	fmt.Printf("Part One: After 2022 rocks have landed, the rock has reached height %d\n", maxRockHeight)
+
+	// Part Two
+	// Clearly we cannot simulate 1000000000000 falling rocks.
+	// So we have to rely on the fact that after infinite time,
+	// the pattern of rocks is guaranteed to converge into a cycle.
+
+	// So to solve the problem we have to find
+	// a) the time (measured in amount of rocks landed) to reach the beginning of a cycle
+	// b) the length of the cycle (in rocks landed)
+
+	// A state can be represented by three labels
+	// 1. An encoding of the state of the top 4 rock layer
+	// 2. The index in the "gust" array
+	// 3. The next rock about to fall
+
+	type memoInfo struct {
+		rocksLanded, rockHeight int
+	}
+	stateMemoization := map[state]memoInfo{}
+
+	rockIterator = 0
+	landedRocks = 0
+	maxRockHeight = 0
+	allRock = map[coord]bool{}
+	for x := 0; x < 7; x++ {
+		allRock[coord{x, 0}] = true
+	}
+	rock = newRock(rockIterator, maxRockHeight, rockTypes)
+	rockIterator++
+
+	firstRepetitionSeen := false
+	firstRepeatedState := state{8, 2}
+
+	cycleMemoization := map[int]int{}
+	cycleCounter := 0
+	inCycle := false
+
+	for round := 0; round < 1000000; round++ {
+		windDirection := wind[round%len(wind)]
+		rock = tryMoveLR(rock, allRock, windDirection)
+		rock, err = tryMoveDown(rock, allRock)
+
+		if err != nil {
+			// update the set with all rock locations and max rock height
+			landedRocks++
+			for _, offset := range rock.offsets {
+				allRock[rock.origin.plus(offset)] = true
+				maxRockHeight = max(rock.origin.plus(offset).y, maxRockHeight)
+			}
+
+			currentState := state{
+				// blockStateEncoding: rockState,
+				gustIndex: round % len(wind),
+				nextRock:  rockIterator % len(rockTypes),
+			}
+
+			if firstRepetitionSeen && currentState == firstRepeatedState {
+				firstRepeatedState = currentState
+				fmt.Printf("Since the last %d was seen, %d more rocks landed and %d additional height was added\n",
+					currentState, landedRocks-stateMemoization[currentState].rocksLanded, maxRockHeight-stateMemoization[currentState].rockHeight)
+				break
+			}
+
+			if stateMemoization[currentState].rocksLanded > 0 && !firstRepetitionSeen {
+				firstRepeatedState = currentState
+				firstRepetitionSeen = true
+				inCycle = true
+				fmt.Println("Numbers at first repetition")
+				fmt.Println(maxRockHeight, "height")
+				fmt.Println(landedRocks, "landed rocks")
+			}
+
+			if inCycle {
+				cycleMemoization[cycleCounter] = maxRockHeight
+				cycleCounter++
+			}
+
+			stateMemoization[currentState] = memoInfo{landedRocks, maxRockHeight}
+
+			// generate a new rock
+			rock = newRock(rockIterator%len(rockTypes), maxRockHeight, rockTypes)
+			rockIterator++
+		}
+	}
+	fmt.Println(cycleMemoization[1188] - cycleMemoization[0])
+}
+
+func render(allRock map[coord]bool, maxHeight int, currentRock block) string {
+	aug := map[coord]int{}
+
+	for key := range allRock {
+		aug[key] = 1
+	}
+	for _, o := range currentRock.offsets {
+		aug[currentRock.origin.plus(o)] = -1
+	}
+
+	output := ""
+	for y := maxHeight + 7; y > 0; y-- {
+		output += "|"
+		for x := 0; x < 7; x++ {
+			if aug[coord{x, y}] == 1 {
+				output += "#"
+			} else if aug[coord{x, y}] == -1 {
+				output += "@"
+			} else {
+				output += "."
+			}
+		}
+		output += "|\n"
+	}
+	output += "|"
+	for x := 0; x < 7; x++ {
+		output += "_"
+	}
+	output += "|\n"
+	return output
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func newRock(rockIterator, maxRockHeight int, rockTypes []block) block {
+	rock := rockTypes[rockIterator]
+	rock.origin = coord{2, maxRockHeight + 4}
+	return rock
+}
+
+func tryMoveLR(rock block, allRock map[coord]bool, gust string) block {
+	var dx int
+	if gust == ">" {
+		dx = 1
+	} else {
+		dx = -1
+	}
+
+	for _, offset := range rock.offsets {
+		newCoord := rock.origin.plus(offset)
+		newCoord.x += dx
+		if newCoord.x < 0 || newCoord.x > 6 || allRock[newCoord] {
+			// then it cannot move left to right
+			return rock
+		}
+	}
+	rock.origin.x += dx
+	return rock
+}
+
+func tryMoveDown(rock block, allRock map[coord]bool) (block, error) {
+	for _, offset := range rock.offsets {
+		nc := rock.origin.plus(offset)
+		nc.y--
+		if allRock[nc] || nc.y <= 0 {
+			return rock, errors.New("hit the floor")
+		}
+	}
+	rock.origin.y--
+	return rock, nil
 }
--- 2022/day18/main.go
+++ 2022/day18/main.go
@@ -4,7 +4,18 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
+
+type coord struct {
+	x, y, z int
+}
+
+func dot(c1, c2 coord) int {
+	return c1.x*c2.x + c1.y*c2.y + c1.z*c2.z
+}
 
 func main() {
 	contents, err := os.ReadFile(os.Args[1])
@@ -12,5 +23,155 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(contents)
+	cube := map[coord]bool{}
+	for _, line := range strings.Split(string(contents), "\n") {
+		xyz := strings.Split(line, ",")
+		x, _ := strconv.Atoi(xyz[0])
+		y, _ := strconv.Atoi(xyz[1])
+		z, _ := strconv.Atoi(xyz[2])
+		cube[coord{x, y, z}] = true
+	}
+
+	var sa int
+	for key := range cube {
+		sa += countEmptyNeighbors(key, cube)
+	}
+
+	fmt.Printf("Part One: Cube has an exposed surface area of %d\n", sa)
+
+	// Part two:
+
+	var xs, ys, zs []int
+	for v := range cube {
+		xs = append(xs, v.x)
+		ys = append(ys, v.y)
+		zs = append(zs, v.z)
+	}
+	sort.Ints(xs)
+	sort.Ints(ys)
+	sort.Ints(zs)
+
+	xMin := xs[0]
+	yMin := ys[0]
+	zMin := zs[0]
+
+	cursor := coord{xMin - 2, yMin - 2, zMin - 2}
+	for {
+		cursor.x++
+		if cube[cursor] {
+			break
+		}
+		cursor.y++
+		if cube[cursor] {
+			break
+		}
+		cursor.z++
+		if cube[cursor] {
+			break
+		}
+	}
+	fmt.Println(cursor)
+
+	outerEdge := map[coord]bool{}
+	checked := map[coord]bool{}
+	outerEdge[cursor] = true
+	// breadth first search
+	queue := unvisitedSurfaceNeighbors(cursor, checked, cube)
+	fmt.Println(queue)
+
+	for len(queue) > 0 {
+		// time.Sleep(500 * time.Millisecond)
+		head := queue[0]
+		outerEdge[head] = true
+		queue = append(queue, unvisitedSurfaceNeighbors(head, checked, cube)...)
+		queue = queue[1:]
+		fmt.Println(queue)
+	}
+
+	fmt.Println(outerEdge)
+	fmt.Println(len(outerEdge))
+
+	var ea int
+	for key := range outerEdge {
+		ea += countEmptyNeighbors(key, cube)
+	}
+
+	fmt.Println("size of the outer edge is ", ea)
+}
+
+func unvisitedSurfaceNeighbors(c coord, checked map[coord]bool, cube map[coord]bool) []coord {
+	out := []coord{}
+	allNeighbors := generateNeighbors(c)
+	for _, n := range allNeighbors {
+		if !checked[n] && countEmptyNeighbors(n, cube) > 0 && cube[n] {
+			checked[n] = true
+			out = append(out, n)
+		}
+	}
+	checked[c] = true
+	return out
+}
+
+func generateNeighbors(c coord) []coord {
+	out := []coord{}
+	for _, dx := range []int{-1, 0, 1} {
+		for _, dy := range []int{-1, 0, 1} {
+			for _, dz := range []int{-1, 0, 1} {
+				out = append(out, coord{c.x + dx, c.y + dy, c.z + dz})
+
+			}
+		}
+	}
+	return out
+}
+
+func countEmptyNeighbors(c coord, cube map[coord]bool) int {
+	base := 6
+	for _, dx := range []int{-1, 1} {
+		if cube[coord{c.x + dx, c.y, c.z}] {
+			base--
+		}
+	}
+	for _, dy := range []int{-1, 1} {
+		if cube[coord{c.x, c.y + dy, c.z}] {
+			base--
+		}
+	}
+	for _, dz := range []int{-1, 1} {
+		if cube[coord{c.x, c.y, c.z + dz}] {
+			base--
+		}
+	}
+	return base
+}
+
+func countSignedEmptyNeighbors(c coord, cube map[coord]bool) int {
+	signedSa := 0
+	for _, dx := range []int{-1, 1} {
+		dr := coord{dx, 0, 0}
+		if !cube[coord{c.x + dx, c.y, c.z}] {
+			signedSa += sgn(dot(c, dr))
+		}
+	}
+	for _, dy := range []int{-1, 1} {
+		dr := coord{0, dy, 0}
+		if !cube[coord{c.x, c.y + dy, c.z}] {
+			signedSa += sgn(dot(c, dr))
+		}
+	}
+	for _, dz := range []int{-1, 1} {
+		dr := coord{0, 0, dz}
+		if !cube[coord{c.x, c.y, c.z + dz}] {
+			signedSa += sgn(dot(c, dr))
+		}
+	}
+
+	return signedSa
+}
+
+func sgn(i int) int {
+	if i > 0 {
+		return 1
+	}
+	return -1
 }

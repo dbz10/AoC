43225d1cf940d01d8feb82c449dd2348dcf8b125
Daniel Ben-Zion <dbzion@gmail.com>
2024-11-30 18:03:19 -0500

    well... not so straightforward to get the sample after all

--- 2024/ceol.py
+++ 2024/ceol.py
@@ -9,11 +9,9 @@
 import typer
 import importlib
 from enum import StrEnum, auto
-import tomllib
-import requests
-import logging
-from os import environ
+
 from bench import bench
+from fetch import get_input
 
 YEAR = 2024
 
@@ -48,53 +46,6 @@ def main(
         run_day()
     else:
         day_runner.main(target_file)
-
-
-# SAMPLE_SELECTOR = "body > main > article:nth-child(1) > pre:nth-child(8) > code"  how to do this consistently across different days?
-INPUT_URL_TEMPLATE = "https://adventofcode.com/{year}/day/{day}/input"
-
-
-def get_input(year: int, day: int) -> None:
-    """
-    It seemed messy to get the sample input since it's not obvious what the consistent way to fetch it would be.
-    For now, content myself with fetching the real input. Maybe I'll come back to this.
-    """
-    # check whether input is already populated. for now just assume if there's any content,
-    # it has been correctly retrieved
-    target_file = f"day{day:02d}/input.txt"
-    try:
-        with open(target_file, "r") as f:
-            lines = len(f.readlines())
-            if lines > 0:
-                logging.info(
-                    "Skipping fetching input since it seems to already be materialized. To force re-fetching it, remove the target file: `rm %s`",
-                    target_file,
-                )
-                return
-    except FileNotFoundError:
-        pass
-
-    input_url = INPUT_URL_TEMPLATE.format(year=year, day=day)
-
-    # We need a session cookie from somewhere
-    cookie = environ.get("AOC_SESSION_COOKIE")
-    if cookie is None:
-        try:
-            with open("env.toml", "rb") as f:
-                cookie = tomllib.load(f)["session_cookie"]
-        except FileNotFoundError:
-            pass
-    if cookie is None:
-        raise ValueError(
-            "AoC session cookie was not found in either `AOC_SESSION_COOKIE` environment variable or env.toml::session_cookie."
-        )
-
-    resp = requests.get(input_url, cookies={"session": cookie})
-    resp.raise_for_status()
-
-    puzzle_input = resp.text
-    with open(target_file, "w") as f:
-        f.write(puzzle_input)
 
 
 if __name__ == "__main__":
---
+++ 2024/fetch.py
@@ -0,0 +1,97 @@
+import tomllib
+import requests
+import logging
+from os import environ
+import bs4
+
+INPUT_URL_TEMPLATE = "https://adventofcode.com/{year}/day/{day}/input"
+PUZZLE_URL_TEMPLATE = "https://adventofcode.com/{year}/day/{day}"
+
+
+def get_input(year: int, day: int) -> None:
+    """
+    It seemed messy to get the sample input since it's not obvious what the consistent way to fetch it would be.
+    For now, content myself with fetching the real input. Maybe I'll come back to this.
+    """
+    # check whether input is already populated. for now just assume if there's any content,
+    # it has been correctly retrieved
+    target_file = f"day{day:02d}/input.txt"
+    try:
+        with open(target_file, "r") as f:
+            lines = len(f.readlines())
+            if lines > 0:
+                logging.info(
+                    "Skipping fetching input since it seems to already be materialized. To force re-fetching it, remove the target file: `rm %s`",
+                    target_file,
+                )
+                return
+    except FileNotFoundError:
+        pass
+
+    input_url = INPUT_URL_TEMPLATE.format(year=year, day=day)
+
+    # We need a session cookie from somewhere
+    cookie = environ.get("AOC_SESSION_COOKIE")
+    if cookie is None:
+        try:
+            with open("env.toml", "rb") as f:
+                cookie = tomllib.load(f)["session_cookie"]
+        except FileNotFoundError:
+            pass
+    if cookie is None:
+        raise ValueError(
+            "AoC session cookie was not found in either `AOC_SESSION_COOKIE` environment variable or env.toml::session_cookie."
+        )
+
+    resp = requests.get(input_url, cookies={"session": cookie})
+    resp.raise_for_status()
+
+    puzzle_input = resp.text
+    with open(target_file, "w") as f:
+        f.write(puzzle_input)
+
+
+def get_example(year: int, day: int) -> None:
+    """
+    Ok, so the example usually follows right after the occurrence of the words "For example:"
+    """
+    # check whether sample is already populated. for now just assume if there's any content,
+    # it has been correctly retrieved
+    target_file = f"day{day:02d}/sample.txt"
+    try:
+        with open(target_file, "r") as f:
+            lines = len(f.readlines())
+            if lines > 0:
+                logging.info(
+                    "Skipping fetching input since it seems to already be materialized. To force re-fetching it, remove the target file: `rm %s`",
+                    target_file,
+                )
+                return
+    except FileNotFoundError:
+        pass
+
+    target_url = PUZZLE_URL_TEMPLATE.format(year=year, day=day)
+    # We need a session cookie from somewhere
+    cookie = environ.get("AOC_SESSION_COOKIE")
+    if cookie is None:
+        try:
+            with open("env.toml", "rb") as f:
+                cookie = tomllib.load(f)["session_cookie"]
+        except FileNotFoundError:
+            pass
+    if cookie is None:
+        raise ValueError(
+            "AoC session cookie was not found in either `AOC_SESSION_COOKIE` environment variable or env.toml::session_cookie."
+        )
+
+    resp = requests.get(target_url, cookies={"session": cookie})
+    resp.raise_for_status()
+
+    puzzle_input = resp.body
+    print(puzzle_input)
+    # with open(target_file, "w") as f:
+    #     f.write(puzzle_input)
+
+
+if __name__ == "__main__":
+    get_example(2023, 10)

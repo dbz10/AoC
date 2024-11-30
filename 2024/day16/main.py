9bee36d10e1bce1bed7a29fe9f3734efc7dcf688
Daniel Ben-Zion <dbzion@gmail.com>
2024-11-30 09:22:22 -0500

    more templating

--- 2024/day16/main.py
+++ 2024/day16/main.py
@@ -1,1 +1,15 @@
 input_file = 'sample.txt'
+
+
+def main():
+	print(f'Part 1: {part1(input)}')
+	print(f'Part 2: {part2(input)}')
+
+def part1(input):
+	return
+
+def part2(input):
+	return
+
+if __name__=='__main__':
+	main()

b533f500da500b285f4300da28c68d563f535eec
Daniel Ben-Zion <dbzion@gmail.com>
2024-12-02 08:19:29 -0500

    2024 day 2

--- 2024/ceol.py
+++ 2024/ceol.py
@@ -3,6 +3,7 @@
 # dependencies = [
 #     "requests",
 #     "typer",
+#     "numpy",
 # ]
 # ///
 
--- 2024/day02/main.py
+++ 2024/day02/main.py
@@ -1,15 +1,40 @@
+import numpy as np
+
+
 def main(input_file="sample.txt"):
-    input = open(input_file).read()
+    input = [
+        np.array(list(map(int, z.split(" "))))
+        for z in open(input_file).read().splitlines()
+    ]
     print(f"Part 1: {part1(input)}")
     print(f"Part 2: {part2(input)}")
 
 
 def part1(input):
-    return
+    return sum(condition1(row) for row in input)
 
 
 def part2(input):
-    return
+    return sum(condition2(row) for row in input)
+
+
+def condition1(row) -> bool:
+    deltas = row[1:] - row[:-1]
+    return (
+        ((deltas > 0).all() or (deltas < 0).all())
+        and (1 <= np.abs(deltas)).all()
+        and (np.abs(deltas) <= 3).all()
+    )
+
+
+def condition2(row) -> bool:
+    if condition1(row):
+        return True
+    for removal in range(len(row)):
+        depleted = np.concatenate([row[:removal], row[removal + 1 :]])
+        if condition1(depleted):
+            return True
+    return False
 
 
 if __name__ == "__main__":

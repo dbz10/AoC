8e0bd9e40b74bd7de98711b35fef003f10ef4b45
Daniel Ben-Zion <dbzion@gmail.com>
2025-12-03 18:29:59 -0500

    2025 day 3

--- 2025/src/bin/03.rs
+++ 2025/src/bin/03.rs
@@ -1,13 +1,49 @@
+use aoc2025;
+
 fn main() {
-    println!("hello from 03");
-    println!("Part 1: {}", part1());
-    println!("Part 2: {}", part2());
+    let content: Vec<Vec<u64>> = aoc2025::read_content()
+        .lines()
+        .map(|s| {
+            s.chars()
+                .map(|ch| ch.to_digit(10).unwrap() as u64)
+                .collect()
+        })
+        .collect();
+
+    println!("Part 1: {}", part1(&content));
+    println!("Part 2: {}", part2(&content));
 }
 
-fn part1() -> String {
-    "hello from part 1".to_string()
+fn bank_joltage(batteries: &[u64], n_batteries: usize) -> u64 {
+    let mut start = 0;
+    let mut chosen: Vec<u64> = vec![];
+
+    for remaining in (0..n_batteries).rev() {
+        let end = batteries.len() - remaining;
+
+        let (offset, &max) = batteries[start..end]
+            .iter()
+            .enumerate()
+            .max_by_key(|&(_, v)| v)
+            .unwrap();
+
+        chosen.push(max);
+        start += offset + 1;
+    }
+
+    chosen.into_iter().fold(0, |acc, d| acc * 10 + d)
 }
 
-fn part2() -> String {
-    "hello from part 2".to_string()
+fn part1(contents: &[Vec<u64>]) -> u64 {
+    contents
+        .into_iter()
+        .map(|x: &Vec<u64>| bank_joltage(x, 2))
+        .sum()
+}
+
+fn part2(contents: &[Vec<u64>]) -> u64 {
+    contents
+        .into_iter()
+        .map(|x: &Vec<u64>| bank_joltage(x, 12))
+        .sum()
 }
